2bec75199174b408e9762623210203287aa8e83c
MahfoudAyoub <60109121+MahfoudAyoub@users.noreply.github.com>
2021-01-21 23:45:57 +0100

    first commit

---
+++ src/component/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from "./Navigation"
+
+function Header(){
+    return(
+        <header className="border-b p-3 flex justify-between item-center">
+            <span className="font-bold">
+                appName
+            </span>
+
+            <Navigation/>
+        </header>
+    )
+}
+
+export default Header;
